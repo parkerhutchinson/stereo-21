94ec4c1202bd94c9e087c293650ded21cec4a5ac
Parker Hutchinson <parkerhutchinson@Parkers-MacBook-Pro.local>
2021-11-01 16:52:11 -0700

    getting slide and slides with the mvp fields

--- migrations/slides/20211101232225-slides-init.js
+++ migrations/slides/20211101232225-slides-init.js
@@ -1,21 +1,79 @@
 module.exports.description = 'Initializes the slides component';
 
 module.exports.up = (migration) => {
-  const ctx = migration.createContentType('slides', {
-    name: 'Slides',
+  const slideCtx = migration.createContentType('slide', {
+    name: 'slide',
+    displayField: 'brand'
   });
 
-  ctx.createField('brand', {
+  slideCtx.createField('brand', {
     type: 'Symbol',
     name: 'Brand',
     required: true,
-  })
-  ctx.createField('logo', {
+  });
+
+  slideCtx.createField('logo', {
     name: 'Logo',
     type: 'Link',
     linkType: 'Asset',
     required: true
+  });
+
+  slideCtx.createField('meshScene', {
+    type: 'Link',
+    name: '3D Scene',
+    required: true,
+    linkType: 'Asset'
   })
+
+  slideCtx.createField('colorSchemeSeed', {
+    type: 'Symbol',
+    name: 'Scheme - Primary Color',
+    required: true
+  });
+
+  slideCtx.createField('colorSchemeHighlight', {
+    type: 'Symbol', 
+    name: 'Scheme - Highlight',
+    required: true
+  });
+
+  slideCtx.createField('colorSchemeBioBG', {
+    type: 'Symbol',
+    name: 'Scheme - Bio Background Color',
+    required: true
+  });
+
+  slideCtx.createField('colorSchemeBioText', {
+    type: 'Symbol', 
+    name: 'Scheme - Bio Text Color',
+    required: true
+  });
+
+  const ctx = migration.createContentType('slides', {
+    name: 'Slides',
+    displayField: 'slidesTitle'
+  });
+
+  ctx.createField('slidesTitle', {
+    type: 'Symbol',
+    name: 'Slides Title',
+    required: true
+  });
+
+  ctx.createField('slideRef', {
+    name: 'Slide Entries',
+    type: 'Array',
+    required: true,
+    items: {
+      type: "Link",
+      linkType: "Entry",
+      validations: [{
+        linkContentType: 'slide'
+      }]
+    }
+  });
+  
 };
 
 module.exports.down = (migration) => {
