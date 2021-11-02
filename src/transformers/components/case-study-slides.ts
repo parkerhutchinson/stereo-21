e0e4929c593804278c0918829d51c344a4ad7bd7
Parker Hutchinson <parkerhutchinson@Parkers-MacBook-Pro.local>
2021-11-02 10:30:49 -0700

    this method for transformer scoping on nested components is far far better than what asana has.

--- src/transformers/components/case-study-slides.ts
+++ src/transformers/components/case-study-slides.ts
@@ -1,9 +1,21 @@
 import {TypeSlidesFields} from "../../types/generated/TypeSlides";
+import {TypeSlideFields} from "../../types/generated/TypeSlide";
+import * as Contentful from "contentful";
 
-const caseStudiesTransformer = (data:TypeSlidesFields) => {
+interface Slides {
+  componentId: string,
+  slides: TypeSlideFields[]
+}
+
+const caseStudiesTransformer = (data:TypeSlidesFields):Slides => {
+  // removes fields scoping from the array to provide clean props for slides component
+  const slides = data.slideRef.map((slide:Contentful.Entry<TypeSlideFields>) => ({
+    ...slide.fields
+  }))
+
   return {
     componentId: 'slides',
-
+    slides: slides
   }
 }
 
