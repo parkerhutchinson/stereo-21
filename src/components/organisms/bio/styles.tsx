7f07698ba13626e340e75ee319ced90c74066912
Parker Hutchinson <parkerhutchinson@Parkers-MacBook-Pro.local>
2021-11-02 17:12:30 -0700

    theres always a trick you have to knonw to make svg render its width fluidly. its not enough to just set its width and height fluid.

--- src/components/organisms/bio/styles.tsx
+++ src/components/organisms/bio/styles.tsx
@@ -2,4 +2,11 @@ import styled from "styled-components";
 
 export const BioTab = styled.article`
   width: 100%;
+  background: black;
+  position: relative;
+  svg{
+    display: block;
+    width: 50%;
+    height: auto;
+  }
 `;
