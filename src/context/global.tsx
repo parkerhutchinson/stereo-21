cc177571fdd9fcb9715db946d5d9aeaaddf40321
Parker Hutchinson <parkerhutchinson@Parkers-MacBook-Pro.local>
2021-11-02 18:18:37 -0700

    knowing that i will have to update the colors when slides change I went ahead and implemented the dispatcher and reducer for the global context provider

--- src/context/global.tsx
+++ src/context/global.tsx
@@ -1,22 +1,49 @@
-import React, { useContext, createContext } from "react";
+import React, { useContext, createContext, useReducer } from "react";
 import type { ReactNode } from "react";
 
 type TDefaultData = {
-  siteBackgroundColor: string
-  bioBackgroundColor: string
-  bioTextColor: string
+  state: {
+    siteBackgroundColor: string
+    bioBackgroundColor: string
+    bioTextColor: string
+  }
+  dispatch?: (action: string) => any
 };
 
 export const GlobalContext = createContext({
-  siteBackgroundColor: 'red',
-  bioBackgroundColor: '#0E0718',
-  bioTextColor: '#726985'
+  state: {
+    siteBackgroundColor: 'red',
+    bioBackgroundColor: '#0E0718',
+    bioTextColor: '#726985'
+  }
 });
 
+const UPDATE_TEXT_COLOR = 'UPDATE_TEXT_COLOR';
+const UPDATE_BIO_BACKGROUND_COLOR = 'UPDATE_BIO_BACKGROUND_COLOR';
+const UPDATE_SITE_BACKGROUND_COLOR = 'UPDATE_SITE_BACKGROUND_COLOR';
+
+const colorSchemeReducer = (state: any, action: { type: string, payload: string }) => {
+  switch (action.type) {
+    case UPDATE_TEXT_COLOR:
+      return Object.assign({}, state, {bioTextColor: action.payload});
+      break;
+    case UPDATE_BIO_BACKGROUND_COLOR:
+      return Object.assign({}, state, {bioBackgroundColor: action.payload});
+      break;
+    case UPDATE_SITE_BACKGROUND_COLOR:
+      return Object.assign({}, state, {siteBackgroundColor: action.payload});
+      break;
+    default:
+      return state;
+  }
+}
+
 const GlobalContextProvider = (props: { children: ReactNode }) => {
-  const GlobalContextState:TDefaultData = useContext(GlobalContext);
+  const GlobalContextState: TDefaultData = useContext(GlobalContext);
+  const [state, dispatch] = useReducer(colorSchemeReducer, GlobalContextState);
+  const value = { state, dispatch }
   return (
-    <GlobalContext.Provider value={GlobalContextState}>
+    <GlobalContext.Provider value={value}>
       {props.children}
     </GlobalContext.Provider>
   );
