b57adb4d50535fb6e8fe7d92ce7387ab45c9f84a
Parker Hutchinson <parkerhutchinson@Parkers-MacBook-Pro.local>
2021-11-12 11:33:39 -0800

    some documentation. removed orbitcontrols since Im exporting a scene and can just rotate that instead. which is really the desired animation anyway.

--- src/components/organisms/slides/threeBackground.tsx
+++ src/components/organisms/slides/threeBackground.tsx
@@ -1,16 +1,19 @@
-import { Suspense } from "react";
+import { Suspense, useRef } from "react";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import * as three from "three";
 import {StyledThreeBackground} from "./styles";
 // import { useSpring } from 'react-spring'
-import { OrbitControls } from "@react-three/drei";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const Model = (props:{url:string}) => {
+  const meshRef = useRef<three.Mesh>();
+  // load the mesh using the GLTF Loader
   const gltf = useLoader(GLTFLoader, props.url)
+  // setup animation mixer
   let mixer:three.AnimationMixer;
   if (gltf.animations.length) {
     mixer = new three.AnimationMixer(gltf.scene);
+    // loop through all animations and play them.
     gltf.animations.forEach(clip => {
       const action = mixer.clipAction(clip)
       action.play();
@@ -18,13 +21,22 @@ const Model = (props:{url:string}) => {
   }
 
   useFrame((state, delta) => {
-      mixer?.update(delta)
+    // run animations every frame
+    meshRef.current!.rotation.x += .001;
+    meshRef.current!.rotation.z += .001;
+    mixer?.update(delta)
   });
 
   return (
     <>
+      <ambientLight intensity={.8}/>
       <pointLight intensity={0.5} position={[5, 3, 5]} />
-      <primitive  position={[0, 0, 0]} object={gltf.scene} scale={1} />
+      <primitive 
+        position={[0, 0, 0]}
+        object={gltf.scene} 
+        scale={1}
+        ref={meshRef}
+      />
     </>
   );
 };
@@ -46,8 +58,6 @@ const ThreeBackground = (props:Props) => {
           zoom: 1.5
         }}
       >
-        <OrbitControls autoRotate={true}/>
-        <ambientLight />
         <Suspense fallback={false}>
           <Model url={finalUrls}/>
         </Suspense>
