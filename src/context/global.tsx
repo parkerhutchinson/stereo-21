cdc182b1a1a11829534df337154e84ed857648ee
Parker Hutchinson <parkerhutchinson@Parkers-MacBook-Pro.local>
2022-01-09 10:50:07 -0800

    context update done

--- src/context/global.tsx
+++ src/context/global.tsx
@@ -1,10 +1,11 @@
-import React, { createContext, useReducer, Dispatch} from "react";
+import React, { createContext, useReducer, Dispatch } from "react";
 import type { ReactNode } from "react";
 
 const initialState = {
   caseStudyOpen: false,
   mobilePanel: false,
-  slideMesh: {slideId: -1, url: ''},
+  slideData: { brand: '' },
+  slideMesh: { slideId: -1, url: '' },
   colorScheme: {
     siteBackgroundColor: 'white',
     bioBackgroundColor: '#0E0718',
@@ -23,7 +24,10 @@ type TDefaultData = {
   slideMesh: {
     slideId: number
     url: string
-  },
+  }
+  slideData: {
+    brand: string
+  }
   colorScheme: {
     siteBackgroundColor: string
     bioBackgroundColor: string
@@ -40,12 +44,13 @@ export enum GlobalActions {
   UPDATE_COLOR = 'UPDATE_COLOR',
   OPEN_CASE_STUDY = 'OPEN_CASE_STUDY',
   TOGGLE_MOBILE_PANEL = 'TOGGLE_MOBILE_PANEL',
-  ADD_SLIDE_MESH = 'ADD_SLIDE_MESH'
+  ADD_SLIDE_MESH = 'ADD_SLIDE_MESH',
+  UPDATE_SLIDE_DATA = 'UPDATE_SLIDE_DATA'
 }
 
 type ColorDispatch = {
   type: GlobalActions
-  payload: {[key:string]:any}
+  payload: { [key: string]: any }
 }
 
 export const GlobalContext = createContext<{
@@ -56,25 +61,27 @@ export const GlobalContext = createContext<{
   dispatch: () => null
 });
 
-const colorSchemeReducer = (state:TDefaultData, action: ColorDispatch) => {
+const globalReducer = (state: TDefaultData, action: ColorDispatch) => {
   switch (action.type) {
     case GlobalActions.UPDATE_COLOR:
-      return Object.assign({}, state, {colorScheme: {...action.payload}})
+      return Object.assign({}, state, { colorScheme: { ...action.payload } })
     case GlobalActions.OPEN_CASE_STUDY:
-      return Object.assign({}, state, {caseStudyOpen: action.payload})
+      return Object.assign({}, state, { caseStudyOpen: action.payload })
     case GlobalActions.TOGGLE_MOBILE_PANEL:
-      return Object.assign({}, state, {mobilePanel: action.payload})
+      return Object.assign({}, state, { mobilePanel: action.payload })
     case GlobalActions.ADD_SLIDE_MESH:
-      return Object.assign({}, state, {slideMesh: {...action.payload} })
+      return Object.assign({}, state, { slideMesh: { ...action.payload } })
+    case GlobalActions.UPDATE_SLIDE_DATA:
+      return Object.assign({}, state, { brand: action.payload })
     default:
       return state;
   }
 }
 
 const GlobalContextProvider = (props: { children: ReactNode }) => {
-  const [state, dispatch] = useReducer(colorSchemeReducer, initialState);
+  const [state, dispatch] = useReducer(globalReducer, initialState);
   return (
-    <GlobalContext.Provider value={{state, dispatch}}>
+    <GlobalContext.Provider value={{ state, dispatch }}>
       {props.children}
     </GlobalContext.Provider>
   );
