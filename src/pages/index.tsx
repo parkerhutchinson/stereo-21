82b9528b36a330329611de476239f1fddceb1549
Parker Hutchinson <parkerhutchinson@Parkers-MacBook-Pro.local>
2022-02-11 16:41:09 -0800

    add favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ const Page: NextPage = () => {
       <Head>
         <title>Parker Hutchinson | {meta.title}</title>
         <meta name="description" content={meta.description} />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
         <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"/>
